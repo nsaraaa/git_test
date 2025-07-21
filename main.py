cd848e6f9463acdd650564cf81a53c139ece26b0
nsararaaa <nsaranoor.3@gmail.com>
2025-07-21 12:07:05 +0500

    issues fixed

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
-def add_numbers(x,y):
- return x+y
+from typing import List
 
+def add_numbers(*args: float) -> float:
+    return sum(args)
 
-def sort_list(unsorted_list:List):
- List.sort()
+def sort_list(unsorted_list: List[float]) -> List[float]:
+    return sorted(unsorted_list)
 
-
-if __name__ == __main__:
- print(add_numbers(2,3))
- print(sort_list([2,3,5.2,1])
-
- 
+if __name__ == "__main__":
+    print(add_numbers(2, 3, 4.5))
+    print(sort_list([2, 3, 5.2, 1]))
